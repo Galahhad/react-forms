56c53c4031c2a57196c857abe365ae5c76bf7892
Galahad <steam_team_look@mail.ru>
2022-05-22 19:53:56 +0300

    react-forms

---
+++ src/components/Form.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+function Form() {
+
+  const [text, setText] = useState('');
+  const [send, setSend] = useState(false);
+  const [blank, setBlank] = useState(false);
+
+  const handleText = (e) => {
+    setText(e.target.value);
+    setSend(false);
+    setBlank(false);
+  }
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      setSend(true);
+      setText('');
+      console.log(text);
+  }
+
+  const handleBlur = () => {
+    if (text.trim().length === 0) {
+      setBlank(true);
+      setSend(false);
+    } else {
+      setBlank(false);
+    }
+  }
+
+  return (
+    <>
+    <form className='send_form' onSubmit={handleSubmit}>
+      <input className={`send_input ${send ? 'sended_class' : ''} ${blank ? 'error' : ''}`} value={text} onChange={handleText} onBlur={handleBlur}/>
+      <button className='send_button' type='submit' disabled={text.trim().length === 0 ? true : false}>Отправить</button>
+    </form>
+    {send && <p className='sended'>Сообщение успешно отправено</p>}
+    {blank && <p className='is-error'>Поле ввода не должно быть пустым</p>}
+    </>
+  )
+}
+export default Form;
